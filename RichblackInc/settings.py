6d9205b01bf437678094472e77ced4eb20f51dd5
Syfa Collins <coniplexy@gmail.com>
2024-08-28 18:11:03 +0300

    Render Postgresql Database configuration

--- RichblackInc/settings.py
+++ RichblackInc/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-twmtie@u03c4nj3jxlec-jrhb-ea#4jvcf#i&w*+d873r8ne8*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['54.76.1.72','127.0.0.1']
+# ALLOWED_HOSTS = ['54.76.1.72','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
+
 
 
 # Application definition
@@ -79,15 +81,26 @@ WSGI_APPLICATION = 'RichblackInc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'richblackinc',
+#         'USER': 'richblackinc',
+#         'PASSWORD': 'richblackinc47',
+#         'HOST': 'richblackinc.c3caw4gsqmo4.eu-west-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
+# RENDER POSTGRESQL DATABASE LIVE
+import dj_database_url
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'richblackinc',
-        'USER': 'richblackinc',
-        'PASSWORD': 'richblackinc47',
-        'HOST': 'richblackinc.c3caw4gsqmo4.eu-west-1.rds.amazonaws.com',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
@@ -129,7 +142,7 @@ STATIC_URL = '/static/'
 
 # Add this if not already present
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
@@ -143,12 +156,12 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
 
-AWS_ACCESS_KEY_ID = 'AKIAQE43JUTMGLVBRU4R '
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'richblackinc'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = ' eu-west-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
+# AWS_ACCESS_KEY_ID = 'AKIAQE43JUTMGLVBRU4R '
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'richblackinc'
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = ' eu-west-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
